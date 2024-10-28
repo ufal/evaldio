21902440babb64aa01051d7bf10ba12f467d9a99
Michal Novak <mnovak@ufal.mff.cuni.cz>
2024-10-28 09:45:35 +0100

    fix: propagate the beep sound path into the verify script

--- data_preparation/separate_exercises/verify_split.sh
+++ data_preparation/separate_exercises/verify_split.sh
@@ -3,7 +3,8 @@
 audio_file=$1
 times_file=$2
 excerpt_half_length=$3
-tgt_path=$4
+beep_file=$4
+tgt_path=$5
 
 (
 read -e split_name
@@ -13,7 +14,7 @@ while read -e split_time; do
     excerpt_file="$tgt_path.$split_name.verify_split_excerpt.mp3"
     verify_file="$tgt_path.$split_name.verify_split.mp3"
     ffmpeg -nostdin -i $audio_file -ss $start_time -to $end_time -c copy "$excerpt_file"
-	ffmpeg -nostdin -i $excerpt_file -i beep.mp3 -filter_complex \
+	ffmpeg -nostdin -i $excerpt_file -i $beep_file -filter_complex \
 		"[1:a]adelay="$excerpt_half_length"000|"$excerpt_half_length"000[b]; [0:a][b]amix=inputs=2" \
 		-c:a libmp3lame -q:a 2 $verify_file
 	rm $excerpt_file
