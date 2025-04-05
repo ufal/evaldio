58c695f8be1fb0b6beab067540a55fc9100e26ad
Michal Novak <mnovak@ufal.mff.cuni.cz>
2025-04-05 23:58:50 +0200

    generalizing extracting labels: works for A1 and A2 level now

--- data_preparation/20.extract_labels/prepare_labels_old_exams.py
+++ data_preparation/20.extract_labels/prepare_labels_old_exams.py
@@ -1,14 +1,16 @@
 import argparse
 import json
+import os
 import pandas
 
 CONFIG = {
-    'A2_older': { 
-        'column_names': ['exam_id', 'evaluator_id',
-                 'ex1_response', 'ex1_lexical', 'ex1_grammar',
-                 'ex2_response', 'ex2_questions', 'ex2_lexical', 'ex2_grammar',
-                 'total_phoninter', 'total_score', 'total_percentage'],
-        'header_rows': 1,
+    'A1': {
+        'column_names': ['exam_origname', 'exam_id', 'evaluator_id',
+            'ex1_criterion1_score', 'ex1_criterion2_score', 'ex1_lexgram_score', 'ex1_phonol_score',
+            'ex2_criterion1_score', 'ex2_criterion2_score', 'ex2_socling_score', 'ex2_lexgram_score', 'ex2_phonol_score',
+            'ex3_criterion1_score', 'ex3_criterion2_score',
+            'total_score'],
+        'header_rows': 2,
     },
     'A2': {
         'column_names': ['exam_origname', 'exam_id', 'evaluator_id',
@@ -17,8 +19,24 @@ CONFIG = {
            'ex3_questions_score', 'ex3_lexgram_score',
            'total_interact_score', 'total_cohesion_score', 'total_phonol_score', 'total_score'],
         'header_rows': 3,
-    }
+    },
+    'A2_older': {
+        'column_names': ['exam_id', 'evaluator_id',
+                 'ex1_response', 'ex1_lexical', 'ex1_grammar',
+                 'ex2_response', 'ex2_questions', 'ex2_lexical', 'ex2_grammar',
+                 'total_phoninter', 'total_score', 'total_percentage'],
+        'header_rows': 1,
+    },
 }
+
+def bugfix_for_A1_max_scores(max_scores):
+    # remove the trailing "?" from the max values if present
+    for key in max_scores.index:
+        if isinstance(max_scores[key], str):
+            max_scores[key] = max_scores[key].replace('?', '')
+    # from the 3rd column on, shift the values to the right by 1
+    max_scores.iloc[3:] = max_scores.iloc[2:-1].values
+    max_scores.iloc[2] = 1
 
 
 def process_data(data, exam_type):
@@ -31,21 +49,40 @@ def process_data(data, exam_type):
 
     # delete the "exam_origname" column
     data.drop(columns=['exam_origname'], inplace=True)
+
     # extract the first row which contains the maximum scores and remove it from the data
-    max_scores = data.iloc[0]
+    max_scores = data.iloc[0].copy()
     data.drop(index=0, inplace=True)
+    # bugfix for A1 table
+    if exam_type == 'A1':
+        bugfix_for_A1_max_scores(max_scores)
+
+    # recast all "_score" columns to Int64 to allow for null values
+    score_column = data.columns[data.columns.str.endswith('_score')]
+    data[score_column] = data[score_column].astype('Int64')
+
     # add new columns with percentage values calculated from the scores normalized to the maximum scores
     for key in data.columns:
         if not key.endswith('_score'):
             continue
         # calculate the percentage value
         new_key = key.replace('_score', '_perc')
-        data[new_key] = data[key].astype(float) / max_scores[key]
+        data[new_key] = data[key] / int(max_scores[key])
+
+def get_json_filename(exam_id, audio_dir, output_dir):
+    for f in os.listdir(audio_dir):
+        # Skip files that don't start with the exam_id
+        if not f.startswith(exam_id):
+            continue
+        # Replace the .mp3 extension with .json
+        filename = f.replace('.mp3', '.json')
+        yield f'{output_dir}/{filename}'
 
 # Parse the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('labels_csv', type=str, help='The path to the CSV file containing the labels for the old exams')
 parser.add_argument('output_dir', type=str, help='The directory to output the JSON files to')
+parser.add_argument('--audio-dir', type=str, help='The directory containing the audio files')
 parser.add_argument('--exam-type', type=str, default='A2_older', help='The type of exam to prepare the labels for')
 args = parser.parse_args()
 
@@ -57,19 +94,15 @@ if args.exam_type not in CONFIG:
 # Load the data from the CSV file
 data = pandas.read_csv(args.labels_csv, sep="\t", names=CONFIG[args.exam_type]['column_names'], skiprows=CONFIG[args.exam_type]['header_rows'])
 
-max_scores = process_data(data, args.exam_type)
-print(max_scores)
+process_data(data, args.exam_type)
 
 # Group data by exam_id
 grouped_data = data.groupby('exam_id')
 
 # Output each row to a separate JSON file in the output directory
 for exam_id, group in grouped_data:
-    evaluations = []
-    for _, row in group.iterrows():
-        evaluation_dict = row.to_dict()
-
-        evaluations.append(evaluation_dict)
+    evaluations = [row.to_dict() for _, row in group.iterrows()]
+    #print(evaluations)
     # Create the average evaluation dictionary by averaging the scores across all evaluations
     avg_evaluation = {}
     for key in group.columns:
@@ -81,7 +114,6 @@ for exam_id, group in grouped_data:
         'evaluations': evaluations,
         'avg': avg_evaluation,
     }
-    output_file = f'{args.output_dir}/{exam_id}_anonym_audio.json'
-    with open(output_file, 'w') as f:
-        json.dump(out_dict, f, indent=4)
-
+    for output_file in get_json_filename(exam_id, args.audio_dir, args.output_dir):
+        with open(output_file, 'w') as f:
+            json.dump(out_dict, f, indent=4)
